90e8d6993e693bfaa6fb6cbab3dfea7e90c18f55
Rick Gaiser <rgaiser@gmail.com>
2017-03-01 21:08:56 +0100

    Copy KLOADER.ELF to usb-stick

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+source "config/config.cfg"
+
 ./get-sources.sh || exit -1
 ./build-toolchain.sh || exit -1
 ./build-buildroot.sh || exit -1
 ./build-kernelloader.sh || exit -1
+cp kernelloader/loader/kloader.elf ${USB_BOOT_DIR}/BOOT/KLOADER.ELF || exit -1
 ./build-firmware.sh || exit -1
 ./build-linux.sh || exit -1
 
